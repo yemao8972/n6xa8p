dd4c83bd273ad55f3be84edb5b7bb6762ce17ad9
yemao8972 <137700665+yemao8972@users.noreply.github.com>
2023-07-05 16:15:25 +0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,31 @@
 import { Title } from "solid-start";
-import Counter from "~/components/Counter";
-
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Title>PRIVACY POLICY</Title>
+	  <h3>PRIVACY POLICY</h3>
+	  <h3>little Animal Assembly Order</h3>
+		<p>
+		We collect advertising IDs for advertising placement <br/>
+
+		We do not have an account registration function in game, so we do not collect any of you<br/>
+		registration information.<br/><br/>
+
+		We do not collect personally identifiable information about you or your child through our<br/>
+		applications. In other words, we do not collect information such as your name,<br/>
+		address, phone number or email address.<br/><br/>		
+
+		We do not have a payment function in game, so we do not collect any of your payment<br/>
+		information.<br/><br/>
+
+		We do not have Push Notifications and Local Notifications functional in game, so we do<br/>
+		not collect any of your notification information.<br/><br/>
+
+		We do not have the function of using location information in game, so we do not collect<br/>
+		any of your location information.<br/><br/>
+
+		have any questions？ email to: hodgeolga45@gmail.com<br/>
+		</p>
     </main>
   );
 }
